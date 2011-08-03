d6df364b069a5bffce2bd6bb5e53e3710b9e7241
Ross Burton <ross@linux.intel.com>
2011-08-03 18:59:06 +0100

    Listen to the signals in the test UI

--- test.c
+++ test.c
@@ -1,5 +1,11 @@
 #include "ap-monitor.h"
 
+static void
+on_update (ApMonitor *monitor, int strength, int tech)
+{
+  g_print ("Strengh %d%% %s\n", strength, ap_monitor_tech_to_string (tech));
+}
+
 int
 main (int argc, char **argv)
 {
@@ -11,6 +17,7 @@ main (int argc, char **argv)
   loop = g_main_loop_new (NULL, TRUE);
 
   apmon = ap_monitor_new ();
+  g_signal_connect (apmon, "update", G_CALLBACK (on_update), NULL);
 
   g_main_loop_run (loop);;
 
